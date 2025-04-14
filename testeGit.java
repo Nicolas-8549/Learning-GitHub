a427083adf358356c187b111fe87a67ded203cab
Nicolas <devnicolasmateus@gmail.com>
2025-04-14 10:42:10 -0300

    c2 - Alterando editor

--- testeGit.java
+++ testeGit.java
@@ -1,6 +1,6 @@
 public class testeGit {
     public static void main(String[] args) {
         System.out.println("Testando se funciona no git");
-        System.out.println("Modificando arquivo");
+        System.out.println("Modificando Arquivo");
     }
 }
